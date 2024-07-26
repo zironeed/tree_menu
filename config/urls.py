ac61d059b72f8d7c6721d19234f7248ba2cdbe9e
Adil Salimgareev <forever@nzironeed.ru>
2024-07-26 15:49:53 +0500

    added template view and url

--- config/urls.py
+++ config/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+from menu_app.views import IndexView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('', IndexView.as_view(), name='index'),
+]
