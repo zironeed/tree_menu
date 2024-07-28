80b6f74b3404e248a72a9960343f8f1e6299e19a
Adil Salimgareev <forever@nzironeed.ru>
2024-07-28 20:10:06 +0500

    added united custom command

---
+++ menu_app/management/commands/configure_project.py
@@ -0,0 +1,16 @@
+from django.core.management import BaseCommand, call_command
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        print('\nThe project configure begins . . .\n')
+        print('manage.py migrate . . .')
+        call_command('migrate')
+        print('Success!')
+        print('manage.py create_admin . . .')
+        call_command('create_admin')
+        print('Success!')
+        print('manage.py create_menu . . .')
+        call_command('create_menu')
+        print('Success!')
