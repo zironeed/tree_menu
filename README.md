3b2a537dcca140da2b62106601bb0da9acee0c47
Adil Salimgareev <forever@nzironeed.ru>
2024-07-28 20:18:51 +0500

    added README.md

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# tree_menu
+# Tree menu
+
+--- 
+
+### Описание
+Tree menu - это django-приложение, реализующее древовидное меню при помощи HTML-шаблонов.
+Возможно редактировать меню через админ-панель Django.
+
+### Стек
+* Python
+* Django
+
+---
+
+### Настройка и запуск проекта
+
+* Активируйте виртуальное окружение:
+```shell
+venv/Scripts/activate
+```
+* Установите миграции:
+```shell
+pip install -r requirements.txt
+```
+* Запустите проект при помощи команды:
+```shell
+python .\manage.py configure_project
+```
+Все готово :)
+
+---
--- menu_app/management/commands/configure_project.py
+++ menu_app/management/commands/configure_project.py
@@ -5,12 +5,18 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         print('\nThe project configure begins . . .\n')
+
         print('manage.py migrate . . .')
         call_command('migrate')
         print('Success!')
+
         print('manage.py create_admin . . .')
         call_command('create_admin')
         print('Success!')
+
         print('manage.py create_menu . . .')
         call_command('create_menu')
         print('Success!')
+
+        print('manage.py runserver . . .')
+        call_command('runserver')
